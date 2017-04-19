7e2ae533666903699dfdf30afed2fee7a3cd2e61
rjconz5 <rjconz5@gmail.com>
2017-04-18 20:19:18 -0500

    Login and Logout added

--- app/src/main/java/com/mightyducklings/cpre186/cashmeoutside/MapsActivity.java
+++ app/src/main/java/com/mightyducklings/cpre186/cashmeoutside/MapsActivity.java
@@ -1,5 +1,6 @@
 package com.mightyducklings.cpre186.cashmeoutside;
 
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Geocoder;
 import android.support.v4.app.ActivityCompat;
@@ -14,15 +15,57 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import static com.mightyducklings.cpre186.cashmeoutside.R.id.buttonLogout;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, View.OnClickListener {
 
     private GoogleMap mMap;
+    //firebase auth object
+    private FirebaseAuth firebaseAuth;
+
+    //view objects
+    private TextView textViewUserEmail;
+    private Button buttonLogout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        //initializing firebase authentication object
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        //if the user is not logged in
+        //that means current user will return null
+        if(firebaseAuth.getCurrentUser() == null){
+            //closing this activity
+            finish();
+            //starting login activity
+            startActivity(new Intent(this, LoginActivity.class));
+        }
+
+        //getting current user
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        //initializing views
+        textViewUserEmail = (TextView) findViewById(R.id.textViewUserEmail);
+        buttonLogout = (Button) findViewById(R.id.buttonLogout);
+
+        //displaying logged in user name
+        textViewUserEmail.setText("Welcome "+user.getEmail());
+
+        //adding listener to button
+        buttonLogout.setOnClickListener(this);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -67,5 +110,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             return;
         }
         mMap.setMyLocationEnabled(true);
+    }
+    public void onClick(View view) {
+        //if logout is pressed
+        if(view == buttonLogout){
+            //logging out the user
+            firebaseAuth.signOut();
+            //closing activity
+            finish();
+            //starting login activity
+            startActivity(new Intent(this, LoginActivity.class));
+        }
     }
 }
